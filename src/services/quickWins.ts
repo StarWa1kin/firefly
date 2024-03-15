bad92c1b16fd624f1aa3dee41ddc9e204b23e897
zhangxiang <zx6@yocyxc.com>
2024-03-15 13:58:25 +0800

    ✨ feat:

--- src/services/quickWins.ts
+++ src/services/quickWins.ts
@@ -29,7 +29,7 @@ interface IDashBoardHistory {
 
 // 返回每小时的选择量，单位为kb（len/8）*1000；
 export function DASHBOARD_GRAPHS(params: IDashBoardGraphs): Promise<API.IQuickWinsInvestigateGraph> {
-  return GET(`/QuickWins/dashboard_graphs`, params);
+  return GET(`/QuickWins/investigate-graph`, params);
 }
 
 // 返回所选天数范围内查询的事件数量
@@ -39,12 +39,12 @@ export function DASHBOARD_INVESTIGATE_LOG(params: IInvestigateLog): Promise<API.
 
 // 返回排名前20的所选天数范围内的查询事件数量
 export function DASHBOARD_HISTORY(params: IDashBoardHistory): Promise<API.IQuickWinsInvestigateLog> {
-  return GET(`/QuickWins/dash_history`, params);
+  return GET(`/QuickWins/dash-history`, params);
 }
 
 // 返回请求的事件代码计数 V3
 export function DASH_BREAKDOWN(params: IDashBoardGraphs): Promise<API.IQuickWinsInvestigateLog[]> {
-  return GET(`/QuickWins/dash_breakdown`, params);
+  return GET(`/QuickWins/dash-breakdown`, params);
 }
 
 // TOTAL 3->3->4
